f6dd6bdf5c331317292aeebbffe30b5c0dbcb520
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2024-05-15 16:35:10 -0500

    Fix feature check for clang-19+
    Applies fix described in https://github.com/xtensor-stack/xtensor/issues/2783

--- include/xtensor/xutils.hpp
+++ include/xtensor/xutils.hpp
@@ -1019,7 +1019,7 @@ namespace xt
         using type = C<X, allocator>;
     };
 
-#if defined(__GNUC__) && __GNUC__ > 6 && !defined(__clang__) && __cplusplus >= 201703L
+#ifdef __cpp_template_template_args
     template <class X, class T, std::size_t N>
     struct rebind_container<X, std::array<T, N>>
     {
